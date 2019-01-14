e90244ac3ee089559b0050706500c32cb966d187
mirzanove <mirzanove@gmail.com>
2019-01-14 08:03:45 +0200

    sssssssss

--- app/src/main/java/mawso3a/noon/mix/MainActivity.java
+++ app/src/main/java/mawso3a/noon/mix/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 // add OK and Cancel buttons
-        builder.setPositiveButton("موافق", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
